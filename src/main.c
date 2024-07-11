7a4fba5fd0f5b7820e3ac9044ad80e2186dce420
arnaudderison <arnaud.derison@hotmail.com>
2024-07-11 21:02:41 +0200

    add new line 🔨

--- src/main.c
+++ src/main.c
@@ -6,13 +6,26 @@
 /*   By: aderison <aderison@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 18:18:56 by aderison          #+#    #+#             */
-/*   Updated: 2024/07/11 18:34:53 by aderison         ###   ########.fr       */
+/*   Updated: 2024/07/11 21:02:04 by aderison         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
+	if (argc <= 1 || (argc == 2 && !argv[1][0]))
+		return (0);
+	if (argc > 2)
+		++argv;
+	if (argv == 2)
+	{
+		argv = ft_split(argv[0], ' ');
+		argv = 0;
+		while (argv[argc])
+			++argc;
+		++argc;
+	}
+	ft_printf("trop cool");
 	return (1);
 }
