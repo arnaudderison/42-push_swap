ca75ce50bfd3eb4949c49531ed7421dc678f9586
arnaudderison <arnaud.derison@hotmail.com>
2024-07-12 00:34:41 +0200

    main 🤓

--- src/main.c
+++ src/main.c
@@ -6,18 +6,23 @@
 /*   By: aderison <aderison@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 18:18:56 by aderison          #+#    #+#             */
-/*   Updated: 2024/07/11 21:02:04 by aderison         ###   ########.fr       */
+/*   Updated: 2024/07/11 21:56:31 by aderison         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "stack.h"
 
 int	main(int argc, char **argv)
 {
+	t_ps	data;
+	int		is_split;
+
 	if (argc <= 1 || (argc == 2 && !argv[1][0]))
 		return (0);
 	if (argc > 2)
 		++argv;
+	is_split = 0;
 	if (argv == 2)
 	{
 		argv = ft_split(argv[0], ' ');
@@ -25,7 +30,13 @@ int	main(int argc, char **argv)
 		while (argv[argc])
 			++argc;
 		++argc;
+		is_split = 0;
 	}
+	init_data(&data, argc, argv);
+	if (is_split > 0)
+		ft_free_matrice(1, &argv);
+	if (argc < 3)
+		return (free_data(&data), 0);
 	ft_printf("trop cool");
 	return (1);
 }
